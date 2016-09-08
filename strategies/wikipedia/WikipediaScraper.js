5ff802a216a2c4991fb78abe7866f883d7e60728
Toby Craig <toby.craig@metaltoad.com>
2016-09-08 11:35:40 -0700

    Removed commented out code and added error handling in Wikipedia scraper

--- strategies/wikipedia/WikipediaScraper.js
+++ strategies/wikipedia/WikipediaScraper.js
@@ -10,14 +10,8 @@ function Scrape(html, more) {
 	var $ = cheerio.load(html);
 	var content = $('#mw-content-text');
 	var para = content.find('p').eq(more ? 1 : 0);
+	if(para.length == 0) {
+		return [{'type': 'text', 'text': 'Sorry, no answer found'}];
+	}
 	return [{'type': 'text', 'text': para.text()}];
-	// var answer = $('.answer').eq(more ? 1 : 0);
-	// if(answer.length == 0) {
-	// 	return [{'type': 'text', 'text': 'Sorry, no answer found'}];
-	// }
-	// var output = [];
-	// answer.find('.post-text').children().each(function(i, el) {
-	// 	output.push({'type': el.tagName == 'pre' ? 'code' : 'text', 'text': $(el).text().trim()});
-	// });
-	// return output;
 }
