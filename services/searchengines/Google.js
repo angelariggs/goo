fa8109a6a70c6e65c3f9688c4ee1ba844df1cf4d
Matt Borowiec <matt.borowiec@metaltoad.com>
2016-09-08 13:07:19 -0700

    Google service queries for the first relevant result and hands off HTML to scraper

--- services/searchengines/Google.js
+++ services/searchengines/Google.js
@@ -1,6 +1,12 @@
 'use strict';
 
 var request = require('request');
+var cheerio = require('cheerio');
+var url = require('url');
+var htmlentities = require('html-entities').AllHtmlEntities;
+var Strategies = require('../../strategies');
+
+
 
 module.exports = {
     Search: Search
@@ -23,10 +29,54 @@ function searchCallback(err, resp, body) {
     }
 
     if (resp.statusCode == 200) {
-        filterResults(body)
+        var results = filterResults(body);
+        var resultHtml = getResultHTML(results);
     }
 }
 
-function filterResults(results) {
-    console.log('IN FILTER::::: ' + results)
+function getResultHTML(results) {
+    if (results.length < 1) return false;
+
+    //TODO make this robust forever. Or at least just do something better than picking first result
+    var options = {
+        url: results[0].href
+    };
+
+    request(options, function(err, resp, body) {
+        switch(resp.request.host) {
+            case 'stackoverflow.com':
+                Strategies.StackOverflow.Scrape(body);
+                break;
+            case 'wikipedia.org':
+                //Strategies.Wikipedia.Scrape(body);
+                break;
+            default:
+                //Cry loudly for an adult
+        }
+    })
+}
+
+function filterResults(html) {
+    var $ = cheerio.load(html);
+    var allResults =  $('.g > h3 > a');
+
+    var entities = new htmlentities();
+    var filteredResults = [];
+    //TODO move somewhere more organized
+    var filterByList = ['wikipedia', 'stackoverflow'];
+
+    //TODO do all of this better idiot
+    allResults.each(function(i, el) {
+        //Skip the first because its not an actual result
+        if (i != 0) {
+            var decordedUrl = url.parse(entities.decode(el.attribs.href), true);
+            var urlObj = url.parse(decordedUrl.query.q)
+            for (var j = 0; j < filterByList.length; j++) {
+                if (urlObj.host.indexOf(filterByList[j]) != -1) {
+                    filteredResults.push(urlObj)
+                }
+            }
+        }
+    });
+    return filteredResults;
 }
