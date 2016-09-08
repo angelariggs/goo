0bdb26aa20726a137f5eaae603e6d7231a3c3702
Toby Craig <toby.craig@metaltoad.com>
2016-09-08 11:14:41 -0700

    Added StackOverflow strategy

--- app.js
+++ app.js
@@ -2,6 +2,13 @@
 
 process.env.QUERY = process.argv.slice(2).join(' ');
 var Services = require('./services');
+var Strategies = require('./strategies');
+var request = require('request');
+var url = 'http://stackoverflow.com/questions/1634115/whats-the-difference-between-git-reset-hard-and-git-reset-merge?noredirect=1&lq=1';
+request(url, function(error, request, body) {
+  console.log(Strategies.StackOverflow.Scrape(body));
+});
+
 var print = require('node-print');
 
 switch(process.env.QUERY) {
