b1ffdcf819fce587deb55946c9090520e474dfaf
Alex Banks <alex.banks0@gmail.com>
2016-09-09 13:22:01 -0700

    LINES

--- app.js
+++ app.js
@@ -2,14 +2,11 @@
 'use strict';
 
 var query = process.argv.slice(2).join(' ');
-
 var request = require('request');
-
 var Services = require('./services');
 var Strategies = require('./strategies');
 
 Services.Cache.init();
-
 
 switch(query) {
   case 'expand':
