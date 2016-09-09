1b8497a30c3306461c08c62d6f7f2e6386077627
Toby Craig <tobycraig@gmail.com>
2016-09-09 13:58:43 -0700

    Added chunking to Wikipedia scraper

--- strategies/wikipedia/WikipediaScraper.js
+++ strategies/wikipedia/WikipediaScraper.js
@@ -9,9 +9,36 @@ module.exports = {
 
 function Scrape(html, n) {
   var $ = cheerio.load(html);
-  var para = $('#mw-content-text > p').eq(n);
-  if(para.length == 0) {
-    return [{'type': 'text', 'text': 'Sorry, no answer found'}];
-  }
-  return [{'type': 'text', 'text': para.text()}];
+  var content = $('#mw-content-text').children();
+  var i = 0;
+  var offset = 0;
+  var output;
+  do {
+    output = getChunk(content.slice(offset));
+    offset += output.count;
+    i++;
+  } while(i <= n);
+  return output.output;
+}
+
+function getChunk(nodes) {
+  var output = [];
+  var length = 0;
+  var count = 0;
+  nodes.each(function(i, el) {
+    count++;
+    if(el.tagName == 'div' || el.tagName == 'table') {
+      return;
+    }
+    var text = cheerio(el).text();
+    output.push({'type': 'text', 'text': text});
+    length += text.length;
+    if(length > 1000) {
+      return false;
+    }
+  });
+  return {
+    output: output,
+    count: count
+  };
 }
