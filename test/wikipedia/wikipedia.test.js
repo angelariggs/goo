4e26d20172ffdd920038cf99f0fdd22e22146b62
Matt Borowiec <matt.borowiec@metaltoad.com>
2016-09-09 15:14:30 -0700

    I FIXED IT AGAIN YOU LITTLE FRIENDS

--- test/wikipedia/wikipedia.test.js
+++ test/wikipedia/wikipedia.test.js
@@ -7,28 +7,27 @@ describe('WikipediaScraper', function() {
     assert.equal(wikipedia.Domain, "en.wikipedia.org")
   });
 
-  it('Should apologize if no results are found', function() {
+  it('Should return empty array if no results are found', function() {
     var output = wikipedia.Scrape('', false);
-    assert.equal(output.length, 1);
-    assert.equal(output[0].text, "Sorry, no answer found")
+    assert.equal(output.length, 0);
   });
 
   it('Should return an array of objects if answer is found', function() {
     var testdata = fs.readFileSync('./test/wikipedia/wiki_answer_testdata.html', 'utf8');
     var output = wikipedia.Scrape(testdata, false);
-    assert.equal(output.length, 1);
+    assert.isAbove(output.length, 0);
     assert.isTrue((output[0].hasOwnProperty('text')));
     assert.isTrue((output[0].hasOwnProperty('type')));
-    assert.notInclude(output[0].text, 'Originally spaghetti was notably long')
+    assert.notInclude(output[0].text, 'In the United States around the end of the 19th century,')
   });
 
   it('Should return the second paragraph ', function() {
     var testdata = fs.readFileSync('./test/wikipedia/wiki_answer_testdata.html', 'utf8');
     var output = wikipedia.Scrape(testdata, true);
-    assert.equal(output.length, 1);
+    assert.isAbove(output.length, 0);
     assert.isTrue((output[0].hasOwnProperty('text')));
     assert.isTrue((output[0].hasOwnProperty('type')));
-    assert.include(output[0].text, 'Originally spaghetti was notably long')
+    assert.include(output[0].text, 'In the United States around the end of the 19th century,')
 
   })
 });
